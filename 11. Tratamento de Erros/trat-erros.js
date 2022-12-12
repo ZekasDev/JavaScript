7b4f3f8b3aba33a47b45f3d1088e4cb6c30269f6
ZekasDev <dannndourado@gmail.com>
2022-12-12 16:46:41 -0300

    JS

--- 11. Tratamento de Erros/trat-erros.js
+++ 11. Tratamento de Erros/trat-erros.js
@@ -1,23 +1,21 @@
-var video = []
-video[1] = []
-video[1]['Nome'] = 'Fullmetal Alchemist'
-video[1]['Categoria'] = 'Anime'
+var video = [];
+video[1] = [];
+video[1]["Nome"] = "Fullmetal Alchemist";
+video[1]["Categoria"] = "Anime";
 
 function getVideo(video) {
-    
-    try {
-    console.log(video[0]['nome'])
-    } 
+  try {
+    console.log(video[0]["nome"]);
+  } catch (e) {
+    console.log("Agora sim podemos tratar esse erro");
+    throw new Error(
+      "Houve um erro mas não se preocupe, estamos trabalhando nisso agora"
+    );
+  } finally {
+    console.log("Sempre passa por aqui (try/catch)");
+  }
 
-    catch(e) {
-    console.log('Agora sim podemos tratar esse erro')
-    }
-    
-    finally {
-        console.log('Sempre passa por aqui (try/catch)')
-    } 
-
-console.log('A aplicação não morreu')
+  console.log("A aplicação não morreu");
 }
 
-getVideo(video)
+getVideo(video);
